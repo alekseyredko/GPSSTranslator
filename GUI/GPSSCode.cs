4c2b72720148c6a56953334f93a79fcd0ffd0e2c
Aleksey Redko <panalex389456@gmail.com>
2018-01-03 17:34:45 +0300

    Tree visualization moved

--- GUI/GPSSCode.cs
+++ GUI/GPSSCode.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-namespace GUI
+namespace Translator
 {
     static class GPSSCode
     {
--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using Microsoft.Msagl.Drawing;
 using Microsoft.Msagl.GraphViewerGdi;
-namespace GUI
+namespace Translator
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -88,23 +88,46 @@ namespace GUI
         //сделать дерево, а не схему сети
         private void VisualizeTree(GPSSNode tree)
         {
-            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            //TreeGridView
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
             //create a viewer object 
-            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            GViewer viewer = new GViewer();
             //create a graph object 
-            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            Graph graph = new Graph("graph");
             //create the graph content 
 
             var vertex = GPSSCode.Vertex;
 
+            //for (int i = 0; i < vertex.Count; i++)
+            //{
+            //    for (int j = 0; j < vertex[i].Children.Count; j++)
+            //    {                   
+            //        if(graph.Edges.Where(x=>x.Source == vertex[i].Name.ToString() && 
+            //            x.Target == vertex[i].Children[j].Name.ToString()).Count() == 0)
+            //        {
+            //            if(vertex[i].Transfers.Count > 1)
+            //            {
+            //                graph.AddEdge(vertex[i].Name.ToString(), vertex[i].Transfers[j].ToString(),
+            //                vertex[i].Children[j].Name.ToString());
+            //            }
+            //            else
+            //            {
+            //                graph.AddEdge(vertex[i].Name.ToString(),
+            //                vertex[i].Children[j].Name.ToString());
+            //            }
+            //        }                   
+            //    }
+            //}
+
             for (int i = 0; i < vertex.Count; i++)
             {
                 for (int j = 0; j < vertex[i].Children.Count; j++)
-                {                   
-                    if(graph.Edges.Where(x=>x.Source == vertex[i].Name.ToString() && 
-                        x.Target == vertex[i].Children[j].Name.ToString()).Count() == 0)
+                {
+                    if (graph.Edges.Where(x => x.Source == vertex[i].Name.ToString() &&
+                         x.Target == vertex[i].Children[j].Name.ToString()).Count() == 0)
                     {
-                        if(vertex[i].Transfers.Count > 1)
+                        if (vertex[i].Transfers.Count > 1)
                         {
                             graph.AddEdge(vertex[i].Name.ToString(), vertex[i].Transfers[j].ToString(),
                             vertex[i].Children[j].Name.ToString());
@@ -114,22 +137,20 @@ namespace GUI
                             graph.AddEdge(vertex[i].Name.ToString(),
                             vertex[i].Children[j].Name.ToString());
                         }
-                    }                   
+                    }
                 }
             }
-            graph.Attr.LayerDirection = LayerDirection.LR;
+
+            graph.Attr.LayerDirection = LayerDirection.TB;
             //bind the graph to the viewer 
             viewer.Graph = graph;
-            //associate the viewer with the form 
-            form.SuspendLayout();
-            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            form.Controls.Add(viewer);
-            form.ResumeLayout();
-            //show the form 
-            form.Show();
+            //associate the viewer with the form            
+            viewer.Dock = DockStyle.Fill;
+           
+            host.Child = viewer;
+            TreeGridView.Children.Add(host);
         }
 
-        //TODO: добавить сохранение кода в txt
         private void CodeSaveItem_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
